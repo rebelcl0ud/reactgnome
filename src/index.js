6fb1eed570034aa6b905f2e9a54ba35f3eaa2088
jovanka <jovanka.placeres@gmail.com>
2018-08-03 22:58:36 +0000

    declare winner; helper fn to be called in Board's render fn to check for winner/update display, update handleClick fn to return early if game won or square occupied already

--- src/index.js
+++ src/index.js
@@ -75,8 +75,12 @@ class Board extends React.Component {
 		};
 	}
 
+	// update; adding calculateWinner(squares) returns early if game won or square occupied
 	handleClick(i) {
 		const squares = this.state.squares.slice();
+		if(calculateWinner(squares) || squares[i]) {
+			return;
+		}
 		squares[i] = this.state.xIsNext ? 'X' : 'O';
 		this.setState({
 			squares: squares,
@@ -109,7 +113,14 @@ class Board extends React.Component {
 
 	render() {
 		// displays player's turn
-		const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+		const winner = calculateWinner(this.state.squares);
+		let status;
+		if(winner) {
+			status = `Winner: ${winner}`;
+		}
+		else {
+			status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+		}
 
 		return (
 			<div>
@@ -149,6 +160,27 @@ class Game extends React.Component {
 			</div>
 		);
 	}
+}
+
+// helper fn to determine winner/ no more turns
+function calculateWinner(squares) {
+	const lines = [
+		[0,1,2],
+		[3,4,5],
+		[6,7,8],
+		[0,3,6],
+		[1,4,7],
+		[2,5,8],
+		[0,4,8],
+		[2,4,6]
+	];
+	for(let i=0; i < lines.length; i++) {
+		const [a,b,c] = lines[i];
+		if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
 // ================================================

75378c38c506945734a19d734b37269f6e8dd9e0
jovanka <jovanka.placeres@gmail.com>
2018-08-03 05:18:19 +0000

    Square components no longer maintain state, receive values from Board informing Board when clicked

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-/** 
+/* 
 Square, Board, Game = react component class/type
 component takes params, aka PROPS(properties)
 returns hierarchy of views, displayed via RENDER method
@@ -12,47 +12,85 @@ ex: <Square /> and the like refers to full component class/type,
 can be used independent
 
 passing props; parents to children
-**/
+*/
 
 // Square^ renders 1 button
+
+/*
+setting state, a way to "remember" things
+they are set in constructors; initializes state
+considered private to React component defined in
+
+Note: JS classes super() needs to be called when defining constructor of subclass
+ALL react component classes that have constructor should start w/ super(props)
+*/
 class Square extends React.Component {
-	// setting state, a way to "remember" things
-	// they are set in constructors; initializes state
-	// considered private to React component defined in
+/** 
+constructor deleted, Square not keeping track of game's state
 
-	// JS classes super() needs to be called when defining constructor of subclass
-	// ALL react component classes that have constructor should start w/ super(props)
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null,
-		};
-	}
-
+onClick prop fn Square can call when clicked
+**/
 	render() {
-		// 
 		return (
 			<button 
 				className="square"
-				// passing function as onClick prop
-				// fires after click, else would fire on re-render 
-
-				// each time clicked Square re-rendered
-				// on update 'X' will display and this.state.value will be 'X'
-				// calling 'setState' in component will auto update child components inside
-				onClick={() => this.setState({value: 'X'})}>
-				{this.state.value}
+				onClick={() => this.props.onClick()}
+			>
+				{this.props.value}
 			</button>
 		);
 	}
 }
 
 // Board(below) renders 9 squares
+/** 
+best approach; parent Board will store game's state instead of Square
+minimizes bugs, easier to refactor
+**/
 class Board extends React.Component {
+	/**
+	collecting data from multiple children or comm btwn children,
+	shared state must be declared in parent component
+
+	parent component can pass state back to children using props,
+	keeping children synced with eachother and parent
+
+	initial state below corresponds with 9 squares above
+	**/
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: Array(9).fill(null),
+		};
+	}
+
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({squares: squares});
+	}
+
+	/*
+	passes data from Board to Square
+	passing prop 'value' to Square
+	*/
+	
+	/**
+	`return <Square value={i} />;` to `return <Square value={this.state.squares[i]} />`
+	Square no longer determined by own state,
+	Board instructs each square of current val ('x', 'o', null) 
+
+	renderSquare() method reads from constructor above where squares array defined
+	
+	adding onClick
+	**/
 	renderSquare(i) {
-		// passes data from Board to Square
-		// passing prop 'value' to Square
-		return <Square value={i} />;
+		return (
+			<Square 
+				value={this.state.squares[i]} 
+				onClick={() => this.handleClick(i)} 
+			/>
+		);		
 	}
 
 	render() {

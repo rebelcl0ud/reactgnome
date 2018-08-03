f9d122d93ec009d9b408d8c7de25aad21bcf6df0
jovanka <jovanka.placeres@gmail.com>
2018-08-03 22:43:09 +0000

    status display; player's turn updated after each play

--- src/index.js
+++ src/index.js
@@ -108,7 +108,8 @@ class Board extends React.Component {
 	}
 
 	render() {
-		const status = "Next player: X";
+		// displays player's turn
+		const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
 		return (
 			<div>

6c83a2469ea6c8fb447884afba0a72d720424e00
jovanka <jovanka.placeres@gmail.com>
2018-08-04 03:52:19 +0000

    move used as key, allows to maintain state btwn renders; what's added/missing/same

--- src/index.js
+++ src/index.js
@@ -80,7 +80,8 @@ class Game extends React.Component {
 		const moves = history.map((step, move) => {
 			const desc = move ? `Go to move # ${move}` : 'Go to game start';
 			return (
-				<li>
+				// key gives unique 'id', allows React to maintain state btwn renders
+				<li key={move}>
 					<button onClick={() => this.jumpTo(move)}>{desc}</button>
 				</li>
 			);
